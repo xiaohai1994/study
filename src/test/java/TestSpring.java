907915ca18d1b563eab0d1b14688f26be044053b
root <747500813@qq.com>
2023-03-18 08:02:36 +0800

    模拟spring

---
+++ src/test/java/TestSpring.java
@@ -0,0 +1,17 @@
+import com.spring.AppContext;
+import com.spring.User;
+import com.spring.service.AnnotationConfigApplicationContext;
+import org.junit.Test;
+
+
+public class TestSpring {
+
+    @Test
+    public void test_Spring(){
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(AppContext.class);
+        User user = (User) annotationConfigApplicationContext.getBean("user");
+//        user.println();
+
+    }
+
+}
